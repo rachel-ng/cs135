11f0e6889e93e34d5e356b8fd635b756d59cbe77
rachel-ng <rachel.ng.135@gmail.com>
2019-09-04 14:01:45 -0400

    proj 1

---
+++ proj_1a/calc2.cpp
@@ -0,0 +1,33 @@
+// Author: Rachel Ng 
+// Course: CSCI-135
+// Assignment: E2.10
+
+// Given the gallons of gas in a tank, fuel efficiency in miles per gallon, and the price of gas per gallon 
+// Prints the cost per 100 miles and how far a car can go with the gas in the tank 
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int s;
+    int n = 0;
+    char op = '+';
+    while (cin >> s) {;
+        if (op == '+') {
+            n += s;
+        }
+        else if (op == '-') {
+            n -= s;
+        }
+        else if (op == ';') {
+            cout << n << endl;
+            n = s;
+        }
+        cin >> op;
+    }
+    cout << n << endl;
+    return 0;
+}
---
+++ proj_1a/sum.cpp
@@ -0,0 +1,22 @@
+// Author: Rachel Ng 
+// Course: CSCI-135
+// Assignment: E2.10
+
+// Given the gallons of gas in a tank, fuel efficiency in miles per gallon, and the price of gas per gallon 
+// Prints the cost per 100 miles and how far a car can go with the gas in the tank 
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int s;
+    int n = 0;
+    while (cin >> s) {;
+        n += s;
+    }
+    cout << n << endl;
+    return 0;
+}
