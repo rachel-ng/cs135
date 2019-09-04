1ff74ff0d75b95ae401d631987958ec75008ccd1
Rachel Ng <rng@stuy.edu>
2019-09-04 15:06:14 -0400

    Update calc2.cpp

--- proj_1a/calc2.cpp
+++ proj_1a/calc2.cpp
@@ -1,9 +1,12 @@
-// Author: Rachel Ng 
-// Course: CSCI-135
-// Assignment: E2.10
+/* 
+Author: Rachel Ng 
+Course: CSCI-136
+Instructor: Zamansky
+Assignment: E2.10
 
-// Given the gallons of gas in a tank, fuel efficiency in miles per gallon, and the price of gas per gallon 
-// Prints the cost per 100 miles and how far a car can go with the gas in the tank 
+Given the gallons of gas in a tank, fuel efficiency in miles per gallon, and the price of gas per gallon 
+Prints the cost per 100 miles and how far a car can go with the gas in the tank 
+*/
 
 
 #include <iostream>
