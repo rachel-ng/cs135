31d846e26623467bec963d797b5fa467e5d32276
rachel-ng <rng@stuy.edu>
2019-09-04 18:36:13 -0400

    fib

--- lab_02/fibonacci.cpp
+++ lab_02/fibonacci.cpp
@@ -10,6 +10,12 @@ Uses an array to store ints
 
 #include <iostream>
 
+/*
+As the numbers approach 2 billions they begin to switch between negative and positive at random. 
+The negative numbers stay the same when the program has been run multiple times separately. 
+It seems like there's a limit on how big an integer can get in C++. 
+*/
+
 int main() {
     // make an array
     int fib[60];
@@ -19,11 +25,8 @@ int main() {
     // and all the following ones can be computed iteratively as
     for (int i = 2; i < 60; i++) {
         fib[i] = fib[i-1] + fib[i-2];
-        std::cout << fib[i] << "\n";
     }
     for (int i = 0; i < 60; i++) {
-        std::cout << i << "\t" << fib[i] << "\n";
+        std::cout << fib[i] << std::endl;
     }
-
-
 }
