8eab55a027073c17a5c53bf0b7502b19dd8700ca
Rachel Ng <rachel.ng.135@gmail.com>
2019-09-19 12:44:49 -0400

    cross

---
+++ lab_04/cross.cpp
@@ -0,0 +1,38 @@
+/* 
+Author: Rachel Ng 
+Course: CSCI-136
+Instructor: Zamansky
+Assignment: Lab 4C 
+
+Takes a dimension
+Prints a diagonal cross of that dimension
+*/
+
+
+#include <iostream>
+
+int main() {
+
+    int size;
+
+    std::cout << "Input size: ";
+    std::cin >> size;
+
+    std::cout << "Shape: " << std::endl;
+
+    for (int h = 0; h < size; h++){ // rows
+        for (int w = 0; w < size; w++){ // cols
+            // diagonal cross pattern
+            if (w == size - h or w == h-1){
+                std::cout << "*"; 
+            }
+            else {
+                std::cout << " ";
+            }
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+
+}
