25df8e3ad2b62a8d74c16d825ccf8805dfe2c5d0
John Tse <jtse63@gmail.com>
2019-10-28 00:33:32 -0400

    has indent called

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@
 #include "funcs.h"
 int main()
 {
+  /*
   std::string test1 = "       test1";
   std::cout << "This should print one with spaces" << '\n' << test1 << std::endl;
   std::cout << "This should print one without spaces" << '\n' << removeLeadingSpaces(test1) << std::endl;
+  */
   std::cout << "This should print code without any indents" << std::endl;
   std::string result = unindent();
+  std::cout << "This should print code with the indents" << std::endl;
+  std::string result2 = indent();
 }
