53ca5a48882a7684dfd48193b6861f21859890d5
rachel-ng <rng@stuy.edu>
2019-11-22 02:15:13 -0500

    bounding

--- proj_3/bot.cpp
+++ proj_3/bot.cpp
@@ -11,13 +11,14 @@ const int MAX_ROBOT_NUM = 50;
 int NUM;          // to remember number or robots
 int ROWS, COLS;   // map dimensions
 
+int BOUND_R = 0;
+int BOUND_C = 0;
 int HEIGHT, WIDTH;
 int SECTORS;
-//const int NEIGHBORS[8][2] = {{-1,0},{0,-1},{0,1},{1,0},{-1,-1},{-1,1},{1,-1},{1,1}};
+
 const int NEIGHBORS[12][2] = {{-1,0},{0,-1},{0,1},{1,0},{-1,-1},{-1,1},{1,-1},{1,1},{-2,0},{0,-2},{0,2},{2,0}};
-const int ADJACENT[4][2] = {{-1,0},{0,-1},{0,1},{1,0}};
-int SECTIONS[4] = {0,0,0,0};
-int FARTHEST[4][2] = {{0,0}, {0,2},{2,0},{2,2}};
+const int ADJC[4][2] = {{-1,0},{0,-1},{0,1},{1,0}};
+double SECTIONS[4] = {0,0,0,0};
 
 vector<Loc> LOCATIONS;  
 vector<Loc> BROKEN_LOC;  
@@ -26,6 +27,36 @@ std::vector<int> MOVE_TO;
 std::vector<int> OPEN_SECTS = {0,1,2,3};
 std::vector<std::vector<int>> TREAD;
 std::vector<std::vector<int>> DEAD;
+
+
+bool in_range (Loc loc) {
+  return (loc.r >= BOUND_R && loc.c >= BOUND_C && loc.r < ROWS && loc.c < COLS);
+}
+
+double manhattanDist(Loc start, Loc target) {
+    return abs(start.c-target.c) + abs(start.r-target.r);
+}
+
+int in_sector(int r, int c) {
+    return (int)((floor(r/HEIGHT) * 2) + floor(c/WIDTH));
+}
+
+int check_kernel (Area &area, Loc loc) {
+    int yeet = 0;
+    int row = loc.r; 
+	int col = loc.c;
+
+    for (int i = 0; i < 12; i++) {
+        if(area.inspect(row + NEIGHBORS[i][0], col + NEIGHBORS[i][1]) == DEBRIS) {
+            yeet += 1;
+        }
+        else if (in_range({row + NEIGHBORS[i][0],col + NEIGHBORS[i][1]})) {
+            DEAD[row + NEIGHBORS[i][0]][col + NEIGHBORS[i][1]] += 1;
+        }
+    }
+    return yeet;
+}
+
 
 /* Initialization procedure, called when the game starts: */
 void onStart(int num, int rows, int cols, double mpr,
@@ -43,31 +74,32 @@ void onStart(int num, int rows, int cols, double mpr,
     MOVE_TO.resize(NUM, -1);
     TREAD.resize(ROWS, std::vector<int>(COLS, 0));
     DEAD.resize(ROWS, std::vector<int>(COLS, 0));
-    
-    for (int i = 0; i < 4; i++) {
-        FARTHEST[i][0] = abs((FARTHEST[i][0] * HEIGHT) - (HEIGHT / 2));
-        FARTHEST[i][1] = abs((FARTHEST[i][0] * WIDTH) - (WIDTH / 2));
-    }
 
     log << "Start!" << endl;
 }
-
-
-double manhattanDist(Loc start, Loc target) {
-    return abs(start.c-target.c) + abs(start.r-target.r);
+void bounds(Area &area) {
+    //bool all_ded_c = true;
+    for (int r = BOUND_R; r < ROWS; r++) {
+        bool all_ded_r = true;
+        for (int c = 0; c < COLS; c++) {
+            int t = TREAD[r][c] > 0 ? 1 : 0; 
+            int d = DEAD[r][c] > 0 ? 1 : 0; 
+            int a = (t == 0 && d == 0) ? ((area.inspect(r,c) != DEBRIS) ? 1 : 0) : 1;
+            all_ded_r = (t > 0 && d > 0 && a > 0) ? all_ded_r : false;
+            //all_ded_c = (c == BOUND_C + 1) ? ((t > 0 && d > 0 && a > 0) ? all_ded_c : false) : all_ded_c;
+        }
+        BOUND_R = (all_ded_r && r == BOUND_R + 1) ? r : BOUND_R; 
+    }
+    //BOUND_C = (all_ded_c) ? BOUND_C + 1 : BOUND_C; 
 }
-
-int in_sector(int r, int c) {
-    return (int)((floor(r/HEIGHT) * 2) + floor(c/WIDTH));
-}
-
-double coverage(int sector) {
+double coverage(Area &area, int sector) {
     double sum = 0.0;
     for (int r = 0; r < HEIGHT; r++) {
         for (int c = 0; c < WIDTH; c++) {
-            sum += TREAD[(HEIGHT * floor(sector / 2)) + r][(WIDTH * (sector % 2)) + c] > 0 ? 1 : 0;
-            sum += DEAD[(HEIGHT * floor(sector / 2)) + r][(WIDTH * (sector % 2)) + c] > 0 ? 1 : 0;
-            sum -= (TREAD[(HEIGHT * floor(sector / 2)) + r][(WIDTH * (sector % 2)) + c] > 0 && DEAD[(HEIGHT * floor(sector / 2)) + r][(WIDTH * (sector % 2)) + c] > 0) ? 1 : 0; 
+            int t = TREAD[(HEIGHT * floor(sector / 2)) + r][(WIDTH * (sector % 2)) + c] > 0 ? 1 : 0; 
+            int d = DEAD[(HEIGHT * floor(sector / 2)) + r][(WIDTH * (sector % 2)) + c] > 0 ? 1 : 0; 
+            int a = (t == 0 && d == 0) ? ((area.inspect(r,c) != DEBRIS) ? 1 : 0) : 1;
+            sum += (t > 0 && d > 0 && a > 0) ? 1:0;
         }
     }
     return sum / (WIDTH * HEIGHT);
@@ -75,7 +107,8 @@ double coverage(int sector) {
 
 /* Deciding robot's next move */
 Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
-	int row = loc.r; // current row and column
+	bounds(area);
+    int row = loc.r; 
 	int col = loc.c;
 	
     LOCATIONS[id] = loc;
@@ -140,74 +173,27 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
                 default: return DOWN;
                 }
             }
-            else if (row + NEIGHBORS[i][0] > -1 && row + NEIGHBORS[i][0] < ROWS && col + NEIGHBORS[i][1] > -1 && col + NEIGHBORS[i][1] < COLS) {
+            else if (in_range({row + NEIGHBORS[i][0],col + NEIGHBORS[i][1]})) {
                 DEAD[row + NEIGHBORS[i][0]][col + NEIGHBORS[i][1]] += 1;
             }
         }
         
         for (int i = 0; i < SECTORS; i++) {
-            SECTIONS[i] = coverage(i);
+            SECTIONS[i] = coverage(area, i);
         }
-        if (MOVE_TO[id] == -1 && SECTIONS[in_sector(row,col)] == 1) {
-            for (int i = 0; i < OPEN_SECTS.size(); i++) {
-                if (OPEN_SECTS[i] == in_sector(row,col)) {
-                    OPEN_SECTS[i] = OPEN_SECTS[OPEN_SECTS.size() - 1];
-                    OPEN_SECTS.pop_back();
-                }
-            }
-            MOVE_TO[id] = OPEN_SECTS.size() == 1 ? OPEN_SECTS[0] : OPEN_SECTS[rand() % (OPEN_SECTS.size()-1)];
-        }
-        /*if (SECTION[in_sector(row,col)] == 1 || MOVE_TO[id] != -1 || in_sector(row,col) != MOVE_TO[id]) {
-            int target_r = FARTHEST[MOVE_TO[id]][0];
-            int target_c = FARTHEST[MOVE_TO[id]][1]; 
-            int pref_r = target_r < row ? -1 : 1;
-            int pref_c = target_c < col ? -1 : 1;
-            pref_r = target_r == row ? 0 : pref_r;
-            pref_c = target_c == col ? 0 : pref_c;
-            
-            if (target_r == row && abs(target_c-col) == 1) {
-                MOVE_TO[id] = -1;
-                return pref_c == -1 ? LEFT : RIGHT; 
-            }
-            if (target_c == col && abs(target_r-row) == 1) {
-                MOVE_TO[id] = -1;
-                return pref_r == -1 ? UP : DOWN; 
-            }
-
-            if (area.inspect(row + pref_r, col) == DEBRIS) {
-                MOVE_TO[id] = -1;
-                return pref_r == -1 ? UP : DOWN;
-            }
-            if (area.inspect(row, col + pref_c) == DEBRIS) {
-                MOVE_TO[id] = -1;
-                return pref_c == -1 ? LEFT : RIGHT;
-            }
-
-            if (pref_r == 0) {
-                return pref_c == -1 ? LEFT : RIGHT;
-            }
-            if (pref_c == 0) {
-                return pref_r == -1 ? UP : DOWN;
-            }
-      
-            switch(rand() % 1) {
-            case 0:
-                return pref_r == -1 ? UP : DOWN;
-            default:
-                return pref_c == -1 ? LEFT : RIGHT;
-            }
-        }
-*/
+        
         int best = -1;
         int bestv = ROWS * COLS;
         for (int i = 0; i < 4; i++) {
-            if (row + ADJACENT[i][0] > -1 && row + ADJACENT[i][0] < ROWS && col + ADJACENT[i][1] > -1 && col + ADJACENT[i][1] < COLS) {
-                best = TREAD[row + ADJACENT[i][0]][col + ADJACENT[i][1]] < bestv ? i : best;
-                bestv = TREAD[row + ADJACENT[i][0]][col + ADJACENT[i][1]] < bestv ? TREAD[row + ADJACENT[i][0]][col + ADJACENT[i][1]] : bestv;
+            if (in_range({row + ADJC[i][0],col + ADJC[i][1]})) {
+                best = TREAD[row + ADJC[i][0]][col + ADJC[i][1]] < bestv ? i : best;
+                bestv = TREAD[row + ADJC[i][0]][col + ADJC[i][1]] < bestv ? TREAD[row + ADJC[i][0]][col + ADJC[i][1]] : bestv;
+            }
+            else if (row+ADJC[i][0] < BOUND_R) {
+                return DOWN;
             }
         }
 
-        // if not at a debris field, move randomly:
         switch(best) {
 		case 0: return UP;
 		case 1: return LEFT;
@@ -236,13 +222,17 @@ void onRobotMalfunction(int id, Loc loc, Area &area, ostream &log) {
 
 void onClockTick(int time, ostream &log) {
 	if (time % 100 == 0) {
-        log << time << " " << endl;
-        for (int i = 0; i < LOCATIONS.size(); i++) {
+        log << time << "\t" << BOUND_R << ", " << BOUND_C<< endl;
+        for (int i = 0; i < 4; i++) {
+           log << SECTIONS[i] << "\t"; 
+        }
+        log << endl;
+        /*for (int i = 0; i < LOCATIONS.size(); i++) {
             log << i << "  " << in_sector(LOCATIONS[i].r,LOCATIONS[i].c) << "->" << MOVE_TO[i] << "\t"; 
             if (i % 4 == 3) {
                 log << endl;
             }
-        }
+        }*/
     }
 }
 
