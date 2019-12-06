59a195932226c7cebd12e08042a4dddcfab10c07
rachel-ng <rng@stuy.edu>
2019-12-06 15:00:55 -0500

    .

--- proj_3/bot.cpp
+++ proj_3/bot.cpp
@@ -57,6 +57,7 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
     for (int i = 0; i < 4; i++) {
         if (map.in_range({row + ADJC[i][0],col + ADJC[i][1]})) {
             if (map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status == DED) {
+                // fix broken robot if you're next to one
                 Field r = map.peek({row + ADJC[i][0],col + ADJC[i][1]});
                 map.update({row + ADJC[i][0],col + ADJC[i][1]},ROBOT,r.robot);
                 map.fixed(r.robot);
@@ -73,7 +74,7 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
     }
     
     if (map.locate(id).dead) {
-        // dead robot
+        // dead robots don't move
 		if (map.locate(id).fixer == -1) {
             map.fix(loc,id);
         }
@@ -84,7 +85,7 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
 		return COLLECT;
 	}
     else if (map.locate(id).fixing != -1) {
-        // if it's targeting a broken robot
+        // targeting a broken robot
         Robot r = map.locate(map.locate(id).fixing);
         int target_r = r.loc.r;
         int target_c = r.loc.c;
@@ -108,11 +109,17 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
         map.update(loc,EMPT);
         
         // if row or col is the same, continue moving in the direction of the broken robot
-        if (pref_r == 0) {
+        if (pref_r == 0 && map.peek(row + pref_r, col + pref_c).status != ROBOT) {
             return pref_c == -1 ? LEFT : RIGHT;
         }
-        if (pref_c == 0) {
+        else if (pref_r == 0) {
+            return map.in_range(row-1, col) ? UP : DOWN; 
+        }
+        if (pref_c == 0 && map.peek(row + pref_r, col + pref_c).status != ROBOT) {
             return pref_r == -1 ? UP : DOWN;
+        }
+        else if (pref_c == 0) {
+            return map.in_range(row, col-1) ? LEFT : RIGHT;
         }
 
         // chooses the field with debris to move to next
@@ -124,10 +131,10 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
         }
                 
         // choose a random spot
-        if (map.in_range({row + pref_r,col})) {
+        if (map.in_range({row + pref_r,col}) && map.peek(row + pref_r, col).status != ROBOT) {
             return pref_r == -1 ? UP : DOWN;
         }
-        if (map.in_range({row,col + pref_c})) {
+        if (map.in_range({row,col + pref_c}) && map.peek(row, col + pref_c).status != ROBOT) {
             return pref_c == -1 ? LEFT : RIGHT;
         }
         
@@ -160,7 +167,6 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
                 default: return DOWN;
                 }
             }
-            //if (map.peek(row + NEIGHBORS[i][0], col + NEIGHBORS[i][1]).status == ROBOT) {
             if (!map.bots({row + NEIGHBORS[i][0], col + NEIGHBORS[i][1]})) {
                 map.treaded({row + NEIGHBORS[i][0], col + NEIGHBORS[i][1]}); 
             }
@@ -176,76 +182,53 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
         
         // if it's out of bounds it continues to move until it's no longer out of bounds
         if (row <= map.b_r() && map.in_range({map.b_r() + 1,col}) && map.bots({row+1,col})) {
-        //(map.peek({row+1,col}).status != ROBOT || map.peek({row+1,col}).status != DED)) {
             return DOWN;
         }
         else if (col <= map.b_c() && map.in_range({row,map.b_c() + 1}) && map.bots({row,col+1})) {
-
-        //(map.peek({row,col+1}).status != ROBOT || map.peek({row,col+1}).status != DED)) {
             return RIGHT;
         }
         else if (col >= map.b_cb() && map.in_range({row,map.b_cb() - 1}) && map.bots({row,col-1})) {
- 
-        //(map.peek({row,col-1}).status != ROBOT || map.peek({row,col-1}).status != DED)) {
             return LEFT;
         }
  
         if (row >= map.b_rb() && map.in_range({map.b_rb() - 1,col}) && map.bots({row-1,col})) {
-        
-        //(map.peek({row-1,col}).status != ROBOT || map.peek({row-1,col}).status != DED)) {
             return UP;
         }
         else if (col <= map.b_c() && map.in_range({row,map.b_c() + 1}) && map.bots({row,col+1})) {
-
-        //(map.peek({row,col+1}).status != ROBOT || map.peek({row,col+1}).status != DED)) {
             return RIGHT;
         }
         else if (col >= map.b_cb() && map.in_range({row,map.b_cb() - 1}) && map.bots({row,col-1})) {
-
-        //(map.peek({row,col-1}).status != ROBOT || map.peek({row,col-1}).status != DED)) {
             return LEFT;
         }
         
         if (col <= map.b_c() && map.in_range({row,map.b_c() + 1}) && map.bots({row,col+1})) {
-
-        //(map.peek({row,col+1}).status != ROBOT || map.peek({row,col+1}).status != DED)) {
             return RIGHT;
         }
         else if (row <= map.b_r() && map.in_range({map.b_r() + 1,col}) && map.bots({row+1,col})) {
-
-        //(map.peek({row+1,col}).status != ROBOT || map.peek({row+1,col}).status != DED)) {
             return DOWN;
         }
         else if (row >= map.b_rb() && map.in_range({map.b_rb() - 1,col}) && map.bots({row-1,col})) {
-
-        //(map.peek({row-1,col}).status != ROBOT || map.peek({row-1,col}).status != DED)) {
             return UP;
         }
         
         if (col >= map.b_cb() && map.in_range({row,map.b_cb() - 1}) && map.bots({row,col-1})) {
-
-        //(map.peek({row,col-1}).status != ROBOT || map.peek({row,col-1}).status != DED)) {
             return LEFT;
         }
         else if (row <= map.b_r() && map.in_range({map.b_r() + 1,col}) && map.bots({row+1,col})) {
-
-        //(map.peek({row+1,col}).status != ROBOT || map.peek({row+1,col}).status != DED)) {
             return DOWN;
         }
         else if (row >= map.b_rb() && map.in_range({map.b_rb() - 1,col}) && map.bots({row-1,col})) {
-
-        //(map.peek({row-1,col}).status != ROBOT || map.peek({row-1,col}).status != DED)) {
             return UP;
         }
 
        
 
         // choose the least treaded on field
-
         int best = -1;
         int bestv = ROWS * COLS;
         for (int i = 0; i < 4; i++) {
             if (map.in_range({row + ADJC[i][0],col + ADJC[i][1]}) && map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status != ROBOT) {
+                // bonus for previous locations + nearby robots 
                 int robonus = comploc(map.peek(row + ADJC[i][0],col + ADJC[i][1]).loc, map.locate(id).ploc) ? (NUM/2) : 0;
                 for (int i = 0; i < 12; i++) {
                     if (map.in_range({row + ADJC[i][0] + NEIGHBORS[i][0], col + ADJC[i][1] + NEIGHBORS[i][1]})) {
@@ -256,27 +239,20 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
                 bestv =  map.peek({row + ADJC[i][0],col + ADJC[i][1]}).tread + robonus < bestv ? map.peek({row + ADJC[i][0],col + ADJC[i][1]}).tread + robonus : bestv;
             }
             else if (row+ADJC[i][0] <= map.b_r()-1 && map.bots({row + ADJC[i][0],col + ADJC[i][1]})) {
-
-            //(map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status != ROBOT || map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status != DED)) {
                 return DOWN;
             }
             else if (row+ADJC[i][0] >= map.b_rb()+1 && map.bots({row + ADJC[i][0],col + ADJC[i][1]})) {
-
-            //(map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status != ROBOT || map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status != DED)) {
                 return UP;
             }
             else if (col+ADJC[i][1] <= map.b_c()-1 && map.bots({row + ADJC[i][0],col + ADJC[i][1]})) {
-
-            //(map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status != ROBOT || map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status != DED)) {
                 return RIGHT;
             }
             else if (col+ADJC[i][1] >= map.b_cb()+1 && map.bots({row + ADJC[i][0],col + ADJC[i][1]})) {
-
-            //(map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status != ROBOT || map.peek({row + ADJC[i][0],col + ADJC[i][1]}).status != DED)) {
                 return LEFT;
             }
         }
 
+        // if there's no place to go just stay there 
         if (best == -1) {
             return COLLECT;
         }
@@ -293,6 +269,7 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
 }
 
 void onRobotMalfunction(int id, Loc loc, Area &area, ostream &log) {
+    // map fix function
     map.fix(loc,id);
     log << "Robot " << map.locate(id).fixer << " to fix " << id << "\t("<< loc.r << ", " << loc.c << ")" << "\t" << map.ded()<< endl;
 }
